63c6d124c3f013b875f01b6c01e419c7c193736f
khoirunnisaaaa <k.nisa11602@gmail.com>
2021-03-15 15:59:52 +0700

    Latihan Minggu2
    Latihan Minggu ke-2

---
+++ Minggu2/latihan/Latihan2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author Khoirunnisa
+ */
+public class Latihan2 {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        String nama = "Indonesia";
+        char abjad = 'a';
+        int a = 6;
+        double b = 6.9;
+        
+        System.out.println(nama);
+        System.out.println(a);
+    }
+}
---
+++ Minggu2/latihan/Latihan3.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author Khoirunnisa
+ */
+        class book {
+            int price, pages;
+            
+            public void set(int price, int pages){
+                this.price = price;
+                this.pages = pages;
+            }
+            
+            public void show(){
+                System.out.println("Books Information");
+                System.out.println("Books price : " + price);
+                System.out.println("Number of pages : " + pages);
+            }
+        }
+        public class Latihan3{
+            public static void main(String[] args){
+                book javabook = new book();
+                javabook.set(60_000, 100);
+                javabook.show();
+            }
+        }
+    
+
