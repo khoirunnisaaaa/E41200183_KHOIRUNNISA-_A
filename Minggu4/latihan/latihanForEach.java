a372e8de45364f5b589c38e8bbbcbbe9fbefb74e
khoirunnisaaaa <k.nisa11602@gmail.com>
2021-03-29 14:35:57 +0700

    Minggu 4

---
+++ Minggu4/latihan/latihanForEach.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author Khoirunnisa
+ */
+public class latihanForEach {
+    public static void main(String[] args){
+        int angka[] = {3, 1, 42, 24, 12};
+        for (int x : angka){
+            System.out.print(x + " ");
+        }
+    }
+}
---
+++ Minggu4/latihan/latihanWhile.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author Khoirunnisa
+ */
+public class latihanWhile {
+    public static void main(String[] args){
+        int i = 0;
+        
+        while (i <= 10){
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        }
+    }
+}
